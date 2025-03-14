320818fe46747ac4bd973933e55be60e9a420cac
RehanSajid08 <rehansajid85@gmail.com>
2025-03-14 07:49:25 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,13 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/digital-health-readiness-assessor/', // Add this line
 }));
+
